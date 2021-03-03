e184ed8c4f1c92af9ce8830f4c65ccf1c3d1cbf7
Andreas Terstegge <Terstegge@fh-aachen.de>
2021-03-03 12:11:34 +0100

    Removed EPS compare in Net.cpp

--- src/Net.cpp
+++ src/Net.cpp
@@ -71,9 +71,12 @@ void Net::update(ElementSet *esp) {
     bool U_diff = fabs(_U - u) > 1e-5;
     bool G_diff = fabs(_Gi - gi) > 1e-5;
     bool I_diff = fabs(_Id - id) > 1e-5;
-    
+    //if (U_diff || G_diff || I_diff ) {
+    // Note that the valuea might be INF, and the
+    // X_diff vars give wrong values!!!!
+        
     // Check if the State of the Net has changed
-    if (U_diff || G_diff || I_diff ) {
+    if (_U != u || _Gi != gi || _Id != id ) {
         _U  = u;
         _Gi = gi;
         _Id = id;
