b1e522476856746addcde077337ab418c64c2f0e
Andreas Terstegge <Terstegge@fh-aachen.de>
2022-04-29 11:24:18 +0200

    Updated ignore files

--- common.mk
+++ common.mk
@@ -10,12 +10,12 @@
 #//
 #///////////////////////////////////////////////
 #//
-# 
+#
 # Configuration section
-CXX           =  g++  # or clang++
-CXXFLAGS      = -std=c++2a -g
-LIB_BUILD_DIR = $(DIGISIM_DIR)/BUILD
-GTEST_DIR     = /usr/local/gtest-1.10.0
+CXX       =  g++  # or clang++
+CXXFLAGS  = -std=c++17 -g
+BUILD_DIR = $(DIGISIM_DIR)/BUILD
+GTEST_DIR = /usr/local/gtest-1.10.0
 
 # Utility to control makefile output
 # (detailed output if VERBOSE defined).
@@ -32,40 +32,38 @@ GTEST_LIB_OPTS    = -L$(GTEST_DIR)/lib -lgtest -lgtest_main -pthread
 # Net2Sim 
 NET2SIM_DIR       = $(DIGISIM_DIR)/Net2Sim
 NET2SIM_SRCS      = $(wildcard $(NET2SIM_DIR)/*.cpp)
-NET2SIM_OBJS      = $(foreach obj, $(NET2SIM_SRCS), $(LIB_BUILD_DIR)/$(notdir $(obj)).o)
+NET2SIM_OBJS      = $(foreach obj, $(NET2SIM_SRCS), $(BUILD_DIR)/$(notdir $(obj)).o)
 NET2SIM_BIN       = $(NET2SIM_DIR)/Net2Sim
-LIB_SRC_DIRS     += $(NET2SIM_DIR)
+SRC_DIRS         += $(NET2SIM_DIR)
 
-# KiCad models
-KICAD_DIR         = $(DIGISIM_DIR)/kicad
-KICAD_GEN_DIR     = $(KICAD_DIR)/gen
-KICAD_IMPL_DIR    = $(KICAD_DIR)/impl
-KICAD_NET_FILES   = $(wildcard $(KICAD_DIR)/*/*.net)
-KICAD_NET_DIRS    = $(dir $(KICAD_NET_FILES))
-KICAD_MODELS      = $(notdir $(KICAD_NET_FILES:.net=))
-KICAD_H_FILES     = $(addsuffix .h,   $(addprefix $(KICAD_GEN_DIR)/, $(KICAD_MODELS)))
-KICAD_SRC_FILES   = $(addsuffix .cpp, $(addprefix $(KICAD_GEN_DIR)/, $(KICAD_MODELS)))
-KICAD_IMPL_FILES  = $(wildcard $(KICAD_IMPL_DIR)/*.cpp)
-KICAD_OBJS        = $(foreach obj, $(KICAD_SRC_FILES),  $(LIB_BUILD_DIR)/$(notdir $(obj)).o)
-KICAD_OBJS       += $(foreach obj, $(KICAD_IMPL_FILES), $(LIB_BUILD_DIR)/$(notdir $(obj)).o)
-LIB_SRC_DIRS     += $(KICAD_GEN_DIR) $(KICAD_IMPL_DIR)
+# KiCad Parts
+PARTS_DIR         = $(DIGISIM_DIR)/parts
+PARTS_SUBDIRS     = $(wildcard $(PARTS_DIR)/*)
+KICAD_NET_FILES   = $(wildcard $(PARTS_DIR)/*/*.net)
+KICAD_MODELS      = $(KICAD_NET_FILES:.net=)
+MODELS_H_FILES    = $(addsuffix .h,   $(KICAD_MODELS))
+MODELS_SRC_FILES  = $(addsuffix .cpp, $(KICAD_MODELS))
+MODELS_IMPL_FILES = $(wildcard $(PARTS_DIR)/*/*.cpp)
+MODELS_OBJS       = $(foreach obj, $(MODELS_SRC_FILES),  $(BUILD_DIR)/$(notdir $(obj)).o)
+MODELS_OBJS      += $(foreach obj, $(MODELS_IMPL_FILES), $(BUILD_DIR)/$(notdir $(obj)).o)
+SRC_DIRS         += $(PARTS_SUBDIRS)
 
 # DigiSim library
 DIGISIM_SRC_DIR   = $(DIGISIM_DIR)/src
 DIGISIM_INC_DIR   = $(DIGISIM_DIR)/include
 DIGISIM_SRC_FILES = $(wildcard $(DIGISIM_SRC_DIR)/*.cpp)
-DIGISIM_OBJS      = $(KICAD_OBJS)
-DIGISIM_OBJS     += $(foreach obj, $(DIGISIM_SRC_FILES), $(LIB_BUILD_DIR)/$(notdir $(obj)).o)
+DIGISIM_OBJS      = $(MODELS_OBJS)
+DIGISIM_OBJS     += $(foreach obj, $(DIGISIM_SRC_FILES), $(BUILD_DIR)/$(notdir $(obj)).o)
 DIGISIM_LIB       = $(DIGISIM_DIR)/libDIGISIM.a
 DIGISIM_LIB_OPTS  = -L$(DIGISIM_DIR) -lDIGISIM -pthread
-LIB_SRC_DIRS     += $(DIGISIM_SRC_DIR)
+SRC_DIRS         += $(DIGISIM_SRC_DIR)
 
 # Tests
 TEST_DIR          = $(DIGISIM_DIR)/tests
 TEST_SRCS         = $(wildcard $(TEST_DIR)/*.cpp)
-TEST_OBJS         = $(foreach obj, $(TEST_SRCS), $(LIB_BUILD_DIR)/$(notdir $(obj)).o)
+TEST_OBJS         = $(foreach obj, $(TEST_SRCS), $(BUILD_DIR)/$(notdir $(obj)).o)
 TEST_BIN          = $(TEST_DIR)/RunTests
-LIB_SRC_DIRS     += $(TEST_DIR)
+SRC_DIRS         += $(TEST_DIR)
 
 # User target
 TARGET_SRC_DIRS  += $(dir $(TARGET_NET_FILE))
@@ -74,12 +72,8 @@ TARGET_SRC_FILES += $(foreach dir, $(TARGET_SRC_DIRS), $(wildcard $(dir)/*.cpp))
 TARGET_OBJS       = $(foreach obj, $(TARGET_SRC_FILES), $(BUILD_DIR)/$(notdir $(obj)).o)
 
 # Include files
-INC_DIRS  += $(KICAD_GEN_DIR)
-INC_DIRS  += $(KICAD_IMPL_DIR)
-INC_DIRS  += $(DIGISIM_INC_DIR)/core
-INC_DIRS  += $(DIGISIM_INC_DIR)/gates
-INC_DIRS  += $(DIGISIM_INC_DIR)/twopoles
-INC_DIRS  += $(DIGISIM_INC_DIR)/threepoles
+INC_DIRS  += $(DIGISIM_INC_DIR)
+INC_DIRS  += $(PARTS_SUBDIRS)
 INC_DIRS  += $(TEST_DIR)
 INC_DIRS  += $(GTEST_INC)
 INC_DIRS  += $(TARGET_INC_DIRS)
@@ -90,14 +84,14 @@ INCLUDES = $(addprefix -I, $(INC_DIRS))
 # RULES SECTION #
 #################
 
-all: build_dir $(NET2SIM_BIN) $(KICAD_H_FILES) $(DIGISIM_LIB) $(TARGET_BIN_FILE)
+all: build_dir $(NET2SIM_BIN) $(MODELS_H_FILES) $(DIGISIM_LIB) $(TARGET_BIN_FILE)
 
 tests: all $(TEST_BIN)
 	$(TEST_BIN)
 
 # Read the dependency files
 -include $(NET2SIM_OBJS:.o=.d)
--include $(KICAD_OBJS:.o=.d)
+-include $(MODELS_OBJS:.o=.d)
 -include $(DIGISIM_OBJS:.o=.d)
 -include $(TEST_OBJS:.o=.d)
 -include $(TARGET_OBJS:.o=.d)
@@ -105,19 +99,19 @@ tests: all $(TEST_BIN)
 # Rule to create build directories
 .PHONY: build_dir
 build_dir :
-	$(HIDE) mkdir -p $(LIB_BUILD_DIR)
+	$(HIDE) mkdir -p $(BUILD_DIR)
 ifneq ($(BUILD_DIR),)
 	$(HIDE) mkdir -p $(BUILD_DIR)
 endif
 
 # Compile rule for DigiSim library
 define compileRulesLib
-.PRECIOUS: $(LIB_BUILD_DIR)/%.cpp.o
-$(LIB_BUILD_DIR)/%.cpp.o : $(1)/%.cpp
+.PRECIOUS: $(BUILD_DIR)/%.cpp.o
+$(BUILD_DIR)/%.cpp.o : $(1)/%.cpp
 	@echo "C++  $$(notdir $$<)"
 	$(HIDE) $$(CXX) $$(CXXFLAGS) $$(INCLUDES) -c -o $$@ $$< -MMD
 endef
-$(foreach srcdir, $(LIB_SRC_DIRS), $(eval $(call compileRulesLib, $(srcdir))))
+$(foreach srcdir, $(SRC_DIRS), $(eval $(call compileRulesLib, $(srcdir))))
 
 # Compile rule for user program
 define compileRules
@@ -147,23 +141,25 @@ $(TARGET_BIN_FILE) : $(TARGET_OBJS) $(DIGISIM_LIB)
 	$(HIDE) $(AR) -rc $@ $^
 
 # Build rules for kicad models in DigiSim library
-define modelBuildRule
-.PRECIOUS: $$(KICAD_GEN_DIR)/%.h
-$$(KICAD_GEN_DIR)/%.h : $$(KICAD_DIR)/$(strip $(1))/%.net
-	@echo "NET  $$(notdir $$<)"
-	$$(HIDE) $$(NET2SIM_BIN) -h $$(KICAD_GEN_DIR)/$(strip $(1)).h -c $$(KICAD_GEN_DIR)/$(strip $(1)).cpp  $$^
-.PRECIOUS: $$(KICAD_GEN_DIR)/%.cpp
-$$(KICAD_GEN_DIR)/%.cpp : $$(KICAD_DIR)/$(strip $(1))/%.net
-	@echo "NET  $$(notdir $$<)"
-	$$(HIDE) $$(NET2SIM_BIN) -h $$(KICAD_GEN_DIR)/$(strip $(1)).h -c $$(KICAD_GEN_DIR)/$(strip $(1)).cpp  $$^
-endef
-$(foreach model, $(KICAD_MODELS), $(eval $(call modelBuildRule, $(model))))
+#define modelBuildRule
+#.PRECIOUS: $$(GEN_DIR)/%.h
+#$$(GEN_DIR)/%.h : $$(GEN_DIR)/$(strip $(1))/%.net
+#	@echo "NET  $$(notdir $$<)"
+#	$$(HIDE) $$(NET2SIM_BIN) -h $$(GEN_DIR)/$(strip $(1)).h -c $$(GEN_DIR)/$(strip $(1)).cpp  $$^
+#.PRECIOUS: $$(GEN_DIR)/%.cpp
+#$$(GEN_DIR)/%.cpp : $$(GEN_DIR)/$(strip $(1))/%.net
+#	@echo "NET  $$(notdir $$<)"
+#	$$(HIDE) $$(NET2SIM_BIN) -h $$(GEN_DIR)/$(strip $(1)).h -c $$(GEN_DIR)/$(strip $(1)).cpp  $$^
+#endef
+#$(foreach model, $(PARTS_SUBDIRS), $(eval $(call modelBuildRule, $(model))))
 
 # Build rules for user kicad models
+
 .PRECIOUS: %.h
 %.h : %.net
 	@echo "NET  $(notdir $<)"
 	$(HIDE) $(NET2SIM_BIN) $^
+
 .PRECIOUS: %.cpp
 %.cpp : %.net
 	@echo "NET  $(notdir $<)"
@@ -172,10 +168,10 @@ $(foreach model, $(KICAD_MODELS), $(eval $(call modelBuildRule, $(model))))
 .PHONY: clean
 clean:
 ifeq ($(TARGET_BIN_FILE),)
-	$(HIDE) rm -rf $(LIB_BUILD_DIR)
+	$(HIDE) rm -rf $(BUILD_DIR)
 	$(HIDE) rm -f  $(NET2SIM_BIN) 
-	$(HIDE) rm -f  $(KICAD_H_FILES)
-	$(HIDE) rm -f  $(KICAD_SRC_FILES)
+	$(HIDE) rm -f  $(MODELS_H_FILES)
+	$(HIDE) rm -f  $(MODELS_SRC_FILES)
 	$(HIDE) rm -f  $(DIGISIM_LIB)
 	$(HIDE) rm -f  $(TEST_BIN)
 else
--- parts/Timer/NE555.h
+++
@@ -1,8 +0,0 @@
-#ifndef _NE555_H_
-#define _NE555_H_
-
-#include "NE555P.h"
-using NE555 = NE555P;
-
-#endif	// _NE555_H_
-
