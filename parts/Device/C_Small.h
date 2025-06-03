54a2f7923d89f0dff7cc66889c7cb54c2005b5c3
Andreas Terstegge <Terstegge@fh-aachen.de>
2025-06-03 17:26:25 +0200

    Added C_Small.

---
+++ parts/Device/C_Small.h
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////
+//
+//  This file is part of
+//      ___  ____  __  __  ___    __    ____
+//     / __)(_  _)(  \/  )/ __)  /__\  (  _ \
+//     \__ \ _)(_  )    (( (__  /(__)\  )(_) )
+//     (___/(____)(_/\/\_)\___)(__)(__)(____/
+//
+//  A simulation library for electronic circuits
+//  See also https://github.com/Terstegge/SimCad
+//  (c) Andreas Terstegge
+//
+/////////////////////////////////////////////////
+//
+#ifndef _C_SMALL_H_
+#define _C_SMALL_H_
+
+#include "C.h"
+using C_Small = C;
+
+#endif	// _C_SMALL_H_
